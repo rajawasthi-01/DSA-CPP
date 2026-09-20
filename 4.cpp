49ac5a9c18c0b30640e3745d9b777a11b4fda18f
Raj Awasthi <awasthiraj024@gmail.com>
2026-09-21 00:00:22 +0530

    Added C++ DSA programs

---
+++ 4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 27;
+    char grade = 'A';
+    float marks = 3.99f;
+    bool isSafe = true;
+
+    cout << "Age is " << age << endl;
+    cout << "Grade is " << grade << endl;
+    cout << "Marks are " << marks << endl;
+    cout << "Is safe? " << isSafe << endl;
+
+    return 0;
+}
---
+++ second.cpp
@@ -0,0 +1,5 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"this is my first code to start DSA"<<endl;
+}
