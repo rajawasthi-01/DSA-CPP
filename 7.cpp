aee430bd5dd6af60fecc6e879d706e0bad4328f2
Raj Awasthi <awasthiraj024@gmail.com>
2026-09-21 12:32:07 +0530

    Updated code

---
+++ 7.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=12 , b=13;
+    cout<<"sum="<<a+b<<endl;
+    cout<<"sub="<<a-b<<endl;
+    cout<<"mul="<<a*b<<endl;
+    cout<<"div="<<a/b<<endl; 
+    cout<<"mod="<<a%b<<endl;   
+    return 0;
+}
