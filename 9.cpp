4c8961adca90ea74970f91bac2a6d87c2149e491
Raj Awasthi <awasthiraj024@gmail.com>
2026-09-23 13:52:03 +0530

    Updated code

---
+++ 9.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n= -29;
+    if(n>=0){
+        cout<<"the number is positive"<<endl;
+    } else{
+        cout<<"the number is negative"<<endl;
+    }
+
+    return 0;
+}
